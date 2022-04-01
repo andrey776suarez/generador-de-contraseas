8753df30bd67c58a33838f89cecf36f42fadf87c
andrey776suarez <99951372+andrey776suarez@users.noreply.github.com>
2022-04-01 08:59:14 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,39 @@
+let Contra = 0
+let numero = 0
+let letra = 0
+let simbol = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Pass=")
+    for (let index = 0; index < 8; index++) {
+        Contra = randint(1, 3)
+        if (Contra == 1) {
+            numero = randint(0, 9)
+            basic.showString("" + (numero))
+        } else if (Contra == 2) {
+            letra = randint(1, 4)
+            if (letra == 1) {
+                basic.showString("A")
+            } else if (letra == 2) {
+                basic.showString("B")
+            } else if (letra == 3) {
+                basic.showString("C")
+            } else if (letra == 4) {
+                basic.showString("D")
+            }
+        } else if (Contra == 3) {
+            simbol = randint(1, 10)
+            if (simbol == 1) {
+                basic.showString("*")
+            } else if (simbol == 2) {
+                basic.showString("*")
+            } else if (simbol == 3) {
+                basic.showString("*")
+            } else if (simbol == 2) {
+                basic.showString("?")
+            }
+        }
+    }
+})
+basic.forever(function () {
+	
+})
